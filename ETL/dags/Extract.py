7fe534b7fe2f595322676f51fd96f00ca4e6e87c
Rogelio Cortez <rcortezrosas@gmail.com>
2025-04-02 16:58:42 -0600

    up

---
+++ ETL/dags/Extract.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import datetime,date
+from airflow.providers.http.sensors.http import HttpSensor
+import json
+
+
+with DAG(dag_id='ETL_weather',description='This dag is to extract , transform and load the date from the API of weather',
+         schedule='@daily',start_date=datetime(2023,10,1),end_date=datetime(2023,10,31),default_args={"depends_on_past":True} ,max_active_runs=1) as dag:
+    
+    # Task 1: Extract data from the API
+
+    extract = BashOperator(task_id='Extract_data',bash_command=
+
