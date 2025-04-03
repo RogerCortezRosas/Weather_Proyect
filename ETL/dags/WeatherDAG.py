2ce372bc7783331c825fabc62bfb5514da0bee5f
Rogelio Cortez <rcortezrosas@gmail.com>
2025-04-02 20:04:40 -0600

    up database

--- ETL/dags/WeatherDAG.py
+++ ETL/dags/WeatherDAG.py
@@ -6,10 +6,12 @@ from airflow.providers.http.sensors.http import HttpSensor
 import json
 
 
-with DAG(dag_id='ETL_weather',description='This dag is to extract , transform and load the date from the API of weather',
+with DAG(dag_id='Weather',description='This dag is to extract , transform and load the date from the API of weather',
          schedule='@daily',start_date=datetime(2023,10,1),end_date=datetime(2023,10,31),default_args={"depends_on_past":True} ,max_active_runs=1) as dag:
     
     # Task 1: Extract data from the API
 
-    extract = BashOperator(task_id='Extract_data',bash_command=
+    extract = BashOperator(task_id='Extract_data',bash_command="echo 'Extracting data from the API'")
+
+    extract
 
